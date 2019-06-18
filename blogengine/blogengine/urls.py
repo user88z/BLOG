35ba56f3410ecefc9e480c1ecf9e52880b276324
Arslan Murtazin <user88z@bk.ru>
2019-06-18 10:21:37 +0300

    add views

--- blogengine/blogengine/urls.py
+++ blogengine/blogengine/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from vievs import hello
+from views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
