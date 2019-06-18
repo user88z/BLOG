bd87d9170cc05f822dc54082d34958f5d77270b0
Arslan Murtazin <user88z@bk.ru>
2019-06-18 10:54:47 +0300

    add html template

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def post_list(request):
-    return HttpResponse('<h1>Hello!!!</h1>')
+    return render(request, 'blog/index.html')
